2226ee4797977cc71b8d06260f5e7cd47f15fd5b
@central <central.nguyen@code88.asia>
2026-04-17 14:40:47 +0700

    update smartscaler v1.1

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,4 +1,10 @@
-GroupVersion = schema.GroupVersion{
-    Group:   "curator.dev",
-    Version: "v1alpha1",
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var GroupVersion = schema.GroupVersion{
+	Group:   "curator.dev",
+	Version: "v1alpha1",
 }
